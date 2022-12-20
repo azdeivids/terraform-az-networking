36a23dc7b67fa9c09ee0fe128388ecd86359f0c3
Deivids Egle <deivids.egle@gmail.com>
2022-12-20 20:56:26 +0000

    Added relevant outputs for the new vnet * subnets.

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,12 +1,12 @@
 output "vnet_address_space" {
   description = "Address space for the new vnet"
-  value = azurerm_virtual_network.vnet.address_space
+  value       = azurerm_virtual_network.vnet.address_space
 }
 output "vnet_id" {
   description = "Id for the new vnet"
-  value = azurerm_virtual_network.vnet.id
+  value       = azurerm_virtual_network.vnet.id
 }
 output "vnet_subnets" {
   description = "Id's of the subnets created within the new vnet"
-  value = local.azurerm_subnets[*].id
+  value       = local.azurerm_subnets[*].id
 }
